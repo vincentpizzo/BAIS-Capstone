946b80f03032ea47ad73fc6facd27dd049207d8e
unknown <vpizzo@PBB-C207-28.iowa.uiowa.edu>
2015-03-10 13:47:19 -0500

    Basic search functionality on Default.aspx

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -1,5 +1,11 @@
 ﻿
 Partial Class _Default
     Inherits System.Web.UI.Page
+    Protected Sub btn_search_Click(sender As Object, e As EventArgs) Handles btn_search.Click
+        Dim searchword As String
 
+        searchword = "Select * From dbo.PlacementDB2 where (Company Like '%" + tb_search.Text.ToString() + "%')"
+
+        SqlDataSource1.SelectCommand = searchword
+    End Sub
 End Class
