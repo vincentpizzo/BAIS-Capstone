acd14f2989c690fce308c4c1c83b61b307176592
Kellen Roberts <kellen-roberts@uiowa.edu>
2015-03-24 15:17:05 -0500

    add mroe role security and page obscurity

--- MasterPage.master.vb
+++ MasterPage.master.vb
@@ -1,5 +1,18 @@
 ﻿
 Partial Class MasterPage
     Inherits System.Web.UI.MasterPage
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If Not Roles.IsUserInRole("r_admin") Then
+            Dim menuItems As MenuItemCollection = NavMenu.Items
+            Dim adminItem As New MenuItem()
+            For Each menuItem As MenuItem In menuItems
+                If menuItem.Text = "Admin" Then
+                    adminItem = menuItem
+                End If
+            Next
+            menuItems.Remove(adminItem)
+        End If
+    End Sub
 End Class
 
