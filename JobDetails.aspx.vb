63f134946dada57629168df5aca45b3f755e4fdf
unknown <vpizzo@PBB-C320R-22.iowa.uiowa.edu>
2015-04-22 20:08:02 -0500

    Huge changes: remove superfluous pages and start from videos

--- JobDetails.aspx.vb
+++
@@ -1,5 +0,0 @@
-﻿
-Partial Class JobDetails
-    Inherits System.Web.UI.Page
-
-End Class
--- User/AddJob.aspx.vb
+++
@@ -1,8 +0,0 @@
-﻿
-Partial Class AddJob
-    Inherits System.Web.UI.Page
-
-    Protected Sub FormView2_PageIndexChanging(sender As Object, e As FormViewPageEventArgs) Handles FormView2.PageIndexChanging
-
-    End Sub
-End Class
